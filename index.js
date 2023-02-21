f7490d78983af53013928b5413d6cb62be3c0981
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2023-02-21 20:18:29 +0200

    add model and path for create and get

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+const Transaction = require("./models/Transaction");
 const app = express();
-const PORT = 4040;
+const PORT = process.env.PORT || 5000;
 
-app.get("/api/test", function (req, res) {
-  res.json("Hello World");
+app.use(cors());
+app.use(express.json());
+
+mongoose.set("strictQuery", true);
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.post("/api/transaction", async (req, res) => {
+  const { name, description, datetime, price } = req.body;
+  const transaction = await Transaction.create({
+    name,
+    description,
+    datetime,
+    price,
+  });
+  res.json(transaction);
+});
+
+app.get("/api/transactions", async (req, res) => {
+  const transactions = await Transaction.find({}).sort({datetime:1});
+  res.json(transactions);
 });
 
 app.listen(PORT, (err) => {
   err ? console.log(err) : console.log(`Listening port ${PORT}`);
 });
+
+
---
+++ models/Transaction.js
@@ -0,0 +1,12 @@
+// const mongoose = require("mongoose");
+const { model, Schema } = require("mongoose");
+
+const TransactionSchema = new Schema({
+  name: { type: String, required: true },
+  price:{type: Number, required: true},
+  description: { type: String, required: true },
+  datetime: { type: Date, default: Date.now },
+});
+
+const TransactionModel = model("Transaction", TransactionSchema);
+module.exports = TransactionModel;
